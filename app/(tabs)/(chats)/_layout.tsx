9ac6c6ae97aec0332492765b7ecbfc408c510c0d
cyag0 <cyag.cesar@gmail.com>
2024-11-27 14:35:12 -0600

    chats

--- app/(tabs)/(chats)/_layout.tsx
+++ app/(tabs)/(chats)/_layout.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 
 const _layout = () => {
   return (
     <Stack
       screenOptions={{
+        headerShown: true,
         headerStyle: {
           backgroundColor: "#1E2A32",
         },
@@ -16,6 +18,28 @@ const _layout = () => {
         name="index"
         options={{
           title: "Chats",
+        }}
+      />
+
+      <Stack.Screen
+        name="[id]/messages"
+        options={(props) => {
+          return {
+            title: props.route.params.title || "Messages",
+            headerRight: () => (
+              <View style={{ flexDirection: "row", gap: 16, marginRight: 12 }}>
+                <TouchableOpacity>
+                  <AntDesign color={"#fff"} name="camera" size={20} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <AntDesign color={"#fff"} name="phone" size={20} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <AntDesign color={"#fff"} name="ellipsis1" size={20} />
+                </TouchableOpacity>
+              </View>
+            ),
+          };
         }}
       />
     </Stack>
